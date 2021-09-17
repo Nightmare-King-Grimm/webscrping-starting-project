777c4df9c394983a49a72b9e307da30b71af61b7
Nightmare-King-Grimm <90000003+Nightmare-King-Grimm@users.noreply.github.com>
2021-09-17 12:58:46 -0500

    Add files via upload

---
+++ MySpyder.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Sep 16 14:07:39 2021
+
+@author: 22gulsethj
+"""
+
+class MySpider(scrapy.Spider):
+    name = 'myspider'
+
+    def start_requests(self):
+        return [scrapy.FormRequest("http://www.example.com/login",
+                                   formdata={'user': 'john', 'pass': 'secret'},
+                                   callback=self.logged_in)]
+
+    def logged_in(self, response):
+        # here you would extract links to follow and return Requests for
+        # each of them, with another callback
+        pass
