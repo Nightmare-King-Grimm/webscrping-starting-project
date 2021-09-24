180de81ca8f2e7b50f399e8b104b17840c52acfd
Nightmare-King-Grimm <90000003+Nightmare-King-Grimm@users.noreply.github.com>
2021-09-24 14:07:43 -0500

    Add files via upload

---
+++ scrape_shell.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Sep 23 12:57:44 2021
+
+@author: 22gulsethj
+"""
+import scrape 
+
+scrapy shell "https://military.wikia.org/wiki/List_of_premodern_combat_weapons" --nolog
