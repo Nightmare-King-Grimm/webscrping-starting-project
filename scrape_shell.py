7f26c46b7a4ad6c414b88a66fb9dc05fb133487e
Nightmare-King-Grimm <90000003+Nightmare-King-Grimm@users.noreply.github.com>
2021-09-29 13:48:05 -0500

    Add files via upload

--- scrape_shell.py
+++ scrape_shell.py
@@ -4,6 +4,15 @@ Created on Thu Sep 23 12:57:44 2021
 
 @author: 22gulsethj
 """
-import scrape 
+import scrapy 
+from scrapy.linkextractors import LinkExtractor
+from scrapy.spiders import CrawlSpider, Rule
 
-scrapy shell "https://military.wikia.org/wiki/List_of_premodern_combat_weapons" --nolog
+class scraper(scrapy.Spider):
+    name = 'military.wikia.org'
+    allowed_domains = ['military.wikia.org']
+    start_urls = ['https://military.wikia.org/']
+
+    def parse(self, response):
+        pass
+ 
